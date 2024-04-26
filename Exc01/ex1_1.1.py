d1c44d4ac6fee178808b1c5b602c00e4edf9b663
Eike Rehwald <rehwalde@gmail.com>
2024-04-26 16:06:31 +0200

    cleaned up 1.1 improved formatting, removed unnecessary stuff

--- Exc01/ex1_1.1.py
+++ Exc01/ex1_1.1.py
@@ -11,17 +11,15 @@ thetaref = theta0 * np.cos(np.sqrt(g/l)*t1)
 
 f1 = lambda t, y1, y2: np.array([y2, -g/l * np.sin(y1)])
 
-def explicitEuler(t, theta0, omega0, g, l):
+def explicit_euler(t, theta0, omega0, g, l):
     theta = [theta0]
     omega = [omega0]
-    y = np.array([theta, omega])
     t_last = t[0]
-    
     for k, t_cur in enumerate(t):
         if k == 0:
             continue
         dt, t_last = t_cur - t_last, t_cur
-        y = [theta[k-1], omega[k-1]] + dt*f1(t_cur, theta[k-1], omega[k-1])
+        y = [theta[k-1], omega[k-1]] + dt * f1(t_cur, theta[k-1], omega[k-1])
         theta.append(y[0]) # selber füllen
         omega.append(y[1]) # selber füllen
     return np.asarray(theta)
@@ -31,12 +29,10 @@ T = 12 * np.pi # stop
 dt = np.pi / 20 # time step
 t2 = np.arange(t0, T, dt)
 
-f2 = lambda t, y: np.array([y[1], -g/l * np.sin(y[0])])
-
-def implicitEuler(t, theta0, omega0, g, l):
-    y = np.zeros((2, len(t))) # fist axis dims, second axis time
-    y[0,0] = theta0
-    y[1,0] = omega0
+def implicit_euler(t, theta_init, omega_init, g, l):
+    y = np.zeros((2, len(t))) # first axis dims, second axis time
+    y[0,0] = theta_init
+    y[1,0] = omega_init
 
     for i, _ in enumerate(t):
         if i == 0:
@@ -47,24 +43,24 @@ def implicitEuler(t, theta0, omega0, g, l):
 
 f3 = lambda t, y1, y2: -g/l * np.sin(y1)
 
-def verletIntegration(t, theta0, omega0, g, l):
+def verlet_integration(t, theta_init, omega_init, g, l):
     y = np.zeros((2, len(t)))
-    y[0,0] = theta0
-    y[1,0] = omega0
+    y[0,0] = theta_init
+    y[1,0] = omega_init
     y[0,1] = y[0,0] + dt * y[1,0] + 0.5 * dt ** 2 * f3(0, y[0,0], y[0,1]) # theta
-    y[1,1] = (y[0,0] - y[0,1]) / (2 * dt) # omega; or is this not possible as you need t-dt, t+dt
-    
+    y[1,1] = (y[0,0] - y[0,1]) / dt # omega
+
     for i, _ in enumerate(t):
         if i in (0, 1):
             continue
-        y[0,i] = 2 * y[0,i-1] - y[0,i-2] + dt ** 2 * f3(0, y[0,i-1], y[1,i-1]) # theta (pos)
-        y[1,i] = (y[0,i-2] - y[0,i]) / (2 * dt) # omega (vel)
+        y[0,i] = 2 * y[0,i-1] - y[0,i-2] + dt ** 2 * f3(i * dt, y[0,i-1], y[1,i-1]) # theta (pos)
+        y[1,i] = (y[0,i-2] - y[0,i]) / (2 * dt) # omega (vel) using midpoint
     return y[0]
 
 
 plt.plot(t1, thetaref, label="Small Angle")
-plt.plot(t1, explicitEuler(t1, theta0, omega0, g, l), label="Explicit Euler")
-plt.plot(t2, implicitEuler(t2, theta0, omega0, g, l), label="Implicit Euler")
-plt.plot(t2, verletIntegration(t2, theta0, omega0, g, l), label="Verlet integration")
+plt.plot(t1, explicit_euler(t1, theta0, omega0, g, l), label="Explicit Euler")
+plt.plot(t2, implicit_euler(t2, theta0, omega0, g, l), label="Implicit Euler")
+plt.plot(t2, verlet_integration(t2, theta0, omega0, g, l), label="Verlet integration")
 plt.legend()
 plt.show()
