aa2afb26062883238b355bd2ca65ce71698c107e
Alex <alexander.zieschang@gmail.com>
2024-04-26 17:51:34 +0200

    completed 1.3

--- Exc01/ex1_1.3.py
+++ Exc01/ex1_1.3.py
@@ -1,5 +1,34 @@
-#\dot{u} - a \nabla^2 u = 0
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.sparse import diags, eye
+from scipy.sparse.linalg import spsolve
+
+n = 9  
 dt = 1
-t0 = 0
-T = 8
-a = 1
+dx = 1
+X = range(n)
+T = range(11)  
+U = np.zeros((n, len(T)))  # Lösungsvektor
+
+# Diskreter 1D Laplace
+L = np.diag(np.ones(n-1), -1) + np.diag(np.ones(n-1), 1) - 2*np.diag(np.ones(n))
+
+alpha = 1  # Diffusion coefficient
+
+# Anfangsbedingung
+U[4, 0] = 8
+
+I = eye(n)
+
+# impliziter Euler
+for i in range(1, len(T)):
+    A = (I - dt * alpha * L / dx**2)
+    U[:, i] = np.linalg.solve(A, U[:, i-1])
+
+for t in [0, 1, 4, 10]: 
+    plt.plot(X, U[:, t], label=f"t={t}")
+plt.legend()
+plt.title("Lösung der Wärme-PDE in 1D")
+plt.xlabel("x")
+plt.ylabel("Temperatur")
+plt.show()
