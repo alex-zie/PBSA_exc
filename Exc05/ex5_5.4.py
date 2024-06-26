e14022e229190319c874aa7e9e4494f5eff90dbc
Eike Rehwald <rehwalde@gmail.com>
2024-06-26 11:12:00 +0200

    initial 5.4 setup

---
+++ Exc05/ex5_5.4.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+x_0 = np.array(0.0, 0.0, 0.8)
+x_1 = np.array(1.0, 0.0, 0.5)
+x_2 = np.array(0.2, 0.2, 1.0)
+x_3 = np.array(1, -1, 1)
+
+xp_0 = np.array(5.2, 3.3, 7.5)
+xp_1 = np.array(7.0, 3.0, 7.0)
+xp_2 = np.array(7.199999999999999, 3.7, 9.5)
+xp_3 = np.array(6.0, 2.5, 5.5)
+
+xpp_0 = np.array(1.6, 5.2, 1.5)
+xpp_1 = np.array(3.0, 5.0, 2.5)
+xpp_2 = np.array(3.0, 6.4, 2.7)
+xpp_3 = np.array(1.0, 6.0, -2.5)
+
+xppp_0 = np.array(5.3, 4.7, 5.4)
+xppp_1 = np.array(8.0, 3.5, 7.5)
+xppp_2 = np.array(6.7, 6.5, 6.6)
+xppp_3 = np.array(5.5, 0.5, 9.0)
